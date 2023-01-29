d6f8d054cc103a44a01208a1bcf23aa2c0ae9d7d
maxwell_law <lchmaxwell@gmail.com>
2023-01-30 01:29:08 +0800

    Change insert only analysed data

--- AzureSQL_DDL.py
+++ AzureSQL_DDL.py
@@ -12,9 +12,6 @@ def connect_asql():
 
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
     return cnxn
-    
-
-
 
 
 #look for historical keywords, reture T/F, keyword_id
@@ -33,8 +30,6 @@ def keyword_hist(kw):
         return True, kw_ls.index(kw)+1
     else:
         return False, None
-
-keyword_hist(kw)
 
 
 #find latest tweets 
@@ -59,88 +54,50 @@ def tweet2query(df, kw_id):
     else: 
         emojis_uc = str(df['Emojis'].encode('unicode-escape')).replace("'","''")
 
-    query = f'''INSERT INTO DBO.tweets 
-        (keyword_id ,
-        userscreenname ,
-        username ,
-        time_stamp ,
-        text_txt  ,
-        embedded_text ,
-        emojis ,
-        comments ,
-        likes ,
-        retweets ,
-        image_link ,
-        tweet_url 
-        ) Values (
-        {kw_id},
-        '{'NULL' if pd.isna(df.UserScreenName) else df.UserScreenName}',
-        '{'NULL' if pd.isna(df.UserName) else df.UserName}',
-        '{df.Timestamp}',
-        '{'NULL' if pd.isna(df.Text) else df.Text}',
-        '{'NULL' if pd.isna(df.Embedded_text) else df.Embedded_text}',
-        '{emojis_uc}' ,
-        {0 if pd.isna(df.Comments) else df.Comments},
-        {0 if pd.isna(df.Likes) else df.Likes},
-        {0 if pd.isna(df.Retweets) else df.Retweets},
-        '{'NULL' if df['Image link'] == '[]' else df['Image link'][2:-2].replace("'","''")}',
-        '{df['Tweet URL']}'
-        )'''
-        
+    query = f'''INSERT INTO tweets (keyword_id, userscreenname, user_name, time_stamp, comments_no, likes_no, retweets_no, text_neg, text_neu, text_pos, text_comp, emoji_sent, tweet_url) VALUES (
+    {kw_id},
+    '{'NULL' if pd.isna(df.UserScreenName) else df.UserScreenName}',
+    '{'NULL' if pd.isna(df.UserName) else df.UserName}',
+    '{df.Timestamp}',
+    {0 if pd.isna(df.Comments) else df.Comments},
+    {0 if pd.isna(df.Likes) else df.Likes},
+    {0 if pd.isna(df.Retweets) else df.Retweets},
+    {df['text_neg']},
+    {df['text_neu']},
+    {df['text_pos']},
+    {df['text_comp']},
+    {df['emoji_sent']},
+    '{df['Tweet URL']}'
+    )'''
     return query
 
 
-#try /n download symbol
- 
-
-
-cnxn = connect_asql()
-cursor = cnxn.cursor()
-for lab, row in df.iterrows():
-    query = tweet2query(row,1)
-    cursor.execute(query) 
-    cnxn.commit()   
-
-
-cursor.execute('SELECT emojis from tweets where tweet_id = 22')
-row = cursor.fetchone()
-print(row)
+def update_records(df, kw_id):
+    cnxn = connect_asql()
+    cursor = cnxn.cursor()
+    for lab, row in df.iterrows():
+        query = tweet2query(row,kw_id)
+        cursor.execute(query) 
+        cnxn.commit()   
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-#Query data
-
+#======================Query data
 query = "SELECT * FROM tweets;"
-
-
-cursor.execute(query) 
-row = cursor.fetchone() 
-df_list = [elem for elem in row]
-df = pd.DataFrame()
-
-
-
-
-while row: 
-    df_ls = []
+def download_from_db(query):
+    cnxn = connect_asql()
+    cursor = cnxn.cursor()
+    row = cursor.fetchone() 
     df_list = [elem for elem in row]
-    df_tem = pd.DataFrame([df_list])
-    df = pd.concat([df,df_tem])
-    row = cursor.fetchone()
+    df = pd.DataFrame()
+    while row: 
+        df_ls = []
+        df_list = [elem for elem in row]
+        df_tem = pd.DataFrame([df_list])
+        df = pd.concat([df,df_tem])
+        row = cursor.fetchone()
+    return df
 
-df.head()
 
 
 
